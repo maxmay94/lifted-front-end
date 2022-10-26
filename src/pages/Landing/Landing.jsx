66b52c8b95363c97ce774e29149a1e99efa8eb1c
maxmay94 <maxmay94@gmail.com>
2022-10-25 20:02:11 -0400

    update style

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,11 @@
-
 const Landing = ({ user }) => {
   return (
-    <main className='text-center text-2xl h-screen bg-gradient-to-b from-gray-900 to-slate-800 text-gray-200'>
-      <div className="mx-20">
-        <h1 className="pt-10">Hey {user ? user.name : 'there'}, what are we lifting today?</h1>
+    <div className='text-center text-2xl h-screen bg-gradient-to-b from-gray-900 to-slate-800 text-gray-200'>
+      <div className="px-20">
+        <br />
+        <h1>Hey {user ? user.name : 'there'}, what are we lifting today?</h1>
       </div>
-    </main>
+    </div>
   )
 }
 
