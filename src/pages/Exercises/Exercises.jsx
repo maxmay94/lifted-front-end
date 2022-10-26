14287dca01e36eeb624c0efdfa713fbef0ca419b
maxmay94 <maxmay94@gmail.com>
2022-10-26 19:45:11 -0400

    add select filter for workouts

--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -5,6 +5,7 @@ import ExerciseTile from '../../components/Exercise/ExerciseTile'
 const Exercises = () => {
   const [exercises, setExercises] = useState([])
   const [muscles, setMuscles] = useState([])
+  const [muscleSelected, setMuscleSelected] = useState("All")
 
   useEffect(() => {
     const fetchExercises = async() => {
@@ -21,39 +22,53 @@ const Exercises = () => {
     fetchExercises()
   }, [])
 
+  const handleChange = evt => {
+    setMuscleSelected(evt.target.value)
+  }
+
   return (
-    <div className='bg-gradient-to-b from-gray-900 to-slate-800 text-slate-200'>
-      {/* <h1 className='text-center text-2xl py-4'>All Exercises</h1> */}
-      {/* <div className='grid gap-y-2 grid-cols-2'> */}
+    <div className='bg-gradient-to-b from-gray-900 to-slate-800 text-slate-200 h-full'>
       <div className='grid text-center'>
-        {/* {exercises &&
-          exercises.map(exercise => (
-            <div key={exercise._id}>
-              <ExerciseTile exercise={exercise} />
-            </div>
-          ))
-        } */}
         {
           muscles &&
-          muscles.map((muscle, i) => (
-            <div key={i}>
-              <h1 className='text-2xl'>{muscle.charAt(0).toUpperCase() + muscle.slice(1)}</h1>
+          <div className='w-3/4 mx-auto m-5 text-black'>
+            <select 
+              name="muscleSelect" 
+              id="type-input"
+              value={muscleSelected}
+              onChange={handleChange}
+              className="rounded"
+            >
+              <option value="All" defaultValue={true}>All Muscle Groups</option>
               {
-                exercises.map(exercise => (
-                  <div key={exercise._id}>
-                    {
-                      exercise.muscle.includes(muscle) &&
-                      <div className='w-3/4 mx-auto'>
-                        <ExerciseTile exercise={exercise} />
-                      </div>
-                    }
-                  </div>
+                muscles&&
+                muscles.map((muscle, i) => (
+                  <option key={i} value={muscle}>{muscle}</option>
                 ))
               }
-              <br />
+            </select>
+          </div>
+        }
+        {
+          exercises &&
+          exercises.map(exercise => (
+            <div key={exercise._id} className="">
+              {
+                muscleSelected === "All" ?
+                <div className='w-3/4 mx-auto'>
+                  <ExerciseTile exercise={exercise} />
+                </div>
+                :
+                exercise.muscle.includes(muscleSelected) &&
+                <div className='w-3/4 mx-auto'>
+                  <ExerciseTile exercise={exercise} />
+                </div>
+              }
+            <br />
             </div>
           ))
         }
+        <br />
         <br />
       </div>
     </div>
