17e0b9f8ed5b9d31bbcffe13e98c96ad70307120
maxmay94 <maxmay94@gmail.com>
2022-10-26 14:31:53 -0400

    sort exercises by muscles used

--- src/components/Exercise/ExerciseTile.jsx
+++ src/components/Exercise/ExerciseTile.jsx
@@ -1,13 +1,19 @@
 const ExerciseTile = ({exercise}) => {
   return (
     <div className="text-center m-2 p-1 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl h-full pt-2">
-      <div className=" pt-1">
-        <img className="h-1/4 max-h-32 mx-auto rounded-xl" 
-          src={exercise.url ? exercise.url : 'https://media2.giphy.com/media/xUPGcKoAYCn5fHK0Zq/giphy.gif?cid=790b76119890b6d87289ae179baa7187685153fcd10ed682&rid=giphy.gif&ct=g'} 
-          alt={exercise.name} 
-        />
-        <p>{exercise.name}</p>
-      </div>
+      <button className="h-full w-full">
+        <div className=" pt-1 px-2">
+          <img className="h-1/4 max-h-32 mx-auto rounded-xl" 
+            src={
+              exercise.url ? 
+                exercise.url 
+                : 'https://media2.giphy.com/media/xUPGcKoAYCn5fHK0Zq/giphy.gif?cid=790b76119890b6d87289ae179baa7187685153fcd10ed682&rid=giphy.gif&ct=g'
+            } 
+            alt={exercise.name} 
+          />
+          <p>{exercise.name}</p>
+        </div>
+      </button>
     </div>
   )
 }
--- src/pages/Exercises/Exercises.jsx
+++ src/pages/Exercises/Exercises.jsx
@@ -4,23 +4,55 @@ import ExerciseTile from '../../components/Exercise/ExerciseTile'
 
 const Exercises = () => {
   const [exercises, setExercises] = useState([])
-  console.log(exercises)
+  const [muscles, setMuscles] = useState([])
+
   useEffect(() => {
     const fetchExercises = async() => {
       const exerciseData = await exerciseService.getAllExercises()
+      let temp = []
+      await exerciseData.forEach(exercise => {
+        exercise.muscle.forEach(muscle => {
+          temp.push(muscle)
+        })
+      })
+      let uniqueMuscles = [...new Set(temp)]
+      setMuscles(uniqueMuscles)
       setExercises(exerciseData)
     }
     fetchExercises()
   }, [])
 
   return (
-    <div className='bg-gradient-to-b from-gray-900 to-slate-800 text-slate-200 h-screen'>
-      <h1 className='text-center text-2xl py-4'>Exercises</h1>
-      <div className='grid gap-y-2 grid-cols-2'>
-        {exercises &&
+    <div className='bg-gradient-to-b from-gray-900 to-slate-800 text-slate-200'>
+      {/* <h1 className='text-center text-2xl py-4'>All Exercises</h1> */}
+      {/* <div className='grid gap-y-2 grid-cols-2'> */}
+      <div className='grid text-center'>
+        {/* {exercises &&
           exercises.map(exercise => (
             <div key={exercise._id}>
               <ExerciseTile exercise={exercise} />
+            </div>
+          ))
+        } */}
+        {
+          muscles &&
+          muscles.map((muscle, i) => (
+            <div key={i}>
+              <h1 className='text-2xl'>{muscle.charAt(0).toUpperCase() + muscle.slice(1)}</h1>
+              {
+                exercises.map(exercise => (
+                  <div key={exercise._id}>
+                    {
+                      exercise.muscle.includes(muscle) &&
+                      <div className='w-3/4 mx-auto'>
+                        <ExerciseTile exercise={exercise} />
+                      </div>
+                    }
+                  </div>
+                ))
+              }
+              <br />
+              <br />
             </div>
           ))
         }
