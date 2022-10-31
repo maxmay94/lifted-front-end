9879a891ba749ecf6de2a04c72debe851e057b28
maxmay94 <maxmay94@gmail.com>
2022-10-31 13:11:03 -0400

    update profile page

--- src/pages/Profiles/Profile.jsx
+++ src/pages/Profiles/Profile.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 const Profile = ({user, handleLogout}) => {
 
   return (
-    <div>
-      <h1>Welcome to your profile page</h1>
-      <Link to="" onClick={handleLogout}>LOG OUT</Link>
+    <div className='text-center'>
+      <h1 className='text-xl font-bold p-2'>Welcome to your profile page {user.name}</h1>
+      <Link to="" className='' onClick={handleLogout}>LOG OUT</Link>
+      <h1 className='p-2 font-mono text-amber-500'>Track Stats</h1>
     </div>
   )
 }
