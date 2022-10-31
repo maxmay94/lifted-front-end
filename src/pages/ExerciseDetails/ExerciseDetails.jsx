d7ee0eebadf191c36e0f6e1e2184a9c77579ed9a
maxmay94 <maxmay94@gmail.com>
2022-10-31 14:08:52 -0400

    update style on exercise details page

--- src/pages/ExerciseDetails/ExerciseDetails.jsx
+++ src/pages/ExerciseDetails/ExerciseDetails.jsx
@@ -1,9 +1,9 @@
 const ExerciseDetails = ({exercise}) => {
   return (
-    <div>
+    <div className="bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl m-5">
       <h1 className="text-center text-2xl p-3">{exercise.name}</h1>
       <img src={exercise.url} className='w-1/2 mx-auto' alt={`animated GIF of ${exercise.name} form`} />
-      <div className="px-10 py-5 text-sm">{exercise.description}</div>
+      <div className="px-10 py-5 text-justify">{exercise.description}</div>
     </div>
   )
 }
