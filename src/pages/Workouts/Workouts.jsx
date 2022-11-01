2b512c5e96b4ea2840c5b6ee4a0bdd53fa7d54b2
maxmay94 <maxmay94@gmail.com>
2022-11-01 14:02:53 -0400

    update style on workouts page

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -4,7 +4,7 @@ const Workouts = ({ workouts, exercise, handleSetExercise }) => {
   console.log(workouts)
   return (
     <div>
-      <div className="sticky top-0 z-30 text-center text-2xl bg-gray-900/90 backdrop-blur-md shadow-md h-full" >
+      <div className="sticky top-0 z-30 text-center text-2xl bg-gray-900/90 backdrop-blur-md shadow-md h-full " >
         <h1 className='p-3'>Workouts</h1>
       </div>
       {/* <h1 className="text-center m-5 text-2xl">Workouts</h1> */}
@@ -15,11 +15,11 @@ const Workouts = ({ workouts, exercise, handleSetExercise }) => {
             <div key={i} className="text-center m-5 p-2 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl text-slate-200">
               <h1 className="underline font-semibold">{workouts[key].name}</h1>
               <p className="m-5 text-xs text-center">{workouts[key].description}</p>
-              <div className="divide-y">
+              <div className="divide-y divide-slate-800">
                 {
                   workouts[key].exercises &&
                   workouts[key].exercises.map((exercise, j) => (
-                    <div key={j} className="text-center mx-3 grid grid-cols-2 py-2 text-sm divide-x divide-dashed">
+                    <div key={j} className="text-center mx-3 grid grid-cols-2 py-2 text-sm divide-x divide-slate-800 divide-dashed">
 
                       <Link to={`/exercise/${exercise._id}`} className="h-full w-full">
                         <button onClick={() => handleSetExercise(exercise._id)}>
