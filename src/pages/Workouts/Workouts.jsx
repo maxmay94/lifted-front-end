c5a9936792d976068501eb39d6b00ce2678669f3
maxmay94 <maxmay94@gmail.com>
2022-11-01 13:39:17 -0400

    update style on workouts page

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -9,19 +9,16 @@ const Workouts = ({ workouts }) => {
           return (
             <div key={i} className="text-center m-5 p-2 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl text-slate-200">
               <h1 className="underline">{workouts[key].name}</h1>
-              <p className="m-5">{workouts[key].description}</p>
-              <div className="divide-y divide-dashed">
+              <p className="m-5 text-xs text-center">{workouts[key].description}</p>
+              <div className="divide-y">
                 {
                   workouts[key].exercises &&
                   workouts[key].exercises.map((exercise, j) => (
-                    <div key={j} className="text-center mx-3 grid grid-cols-5 mt-2">
-                      <div className="col-span-2 text-amber-500">
+                    <div key={j} className="text-center mx-3 grid grid-cols-2 py-2 text-sm divide-x divide-dashed">
+                      <div className=" text-amber-500">
                         {exercise.name}
                       </div>
-                      <div className="col-span-1">
-                        -->
-                      </div>
-                      <div className="col-span-2">
+                      <div className="">
                         {workouts[key].setsReps[j]}
                       </div>
                     </div>
