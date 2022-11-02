1992c3ee75dc44efd9edfb79b6b8d8951d2ce37f
maxmay94 <maxmay94@gmail.com>
2022-11-02 16:52:49 -0400

    minor adjustment to workout detail page

--- src/pages/WorkoutDetails/WorkoutDetails.jsx
+++ src/pages/WorkoutDetails/WorkoutDetails.jsx
@@ -1,18 +1,17 @@
 import WorkoutTile from "../../components/WorkoutTile/WorkoutTile"
 
 const WorkoutDetails = ({workout}) => {
-  console.log(workout)
   return (
     <div>
       <div className="sticky top-0 z-30 text-center text-2xl bg-gray-900/90 backdrop-blur-md shadow-md h-full" >
         <h1 className='p-3'>Workout Detail</h1>
+        </div>
         {
           workout &&
           <div className="text-center m-5 p-2 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl text-slate-200">
             <WorkoutTile workout={workout}/>
           </div>
         }
-      </div>
     </div>
   )
 }
