e2869add5c10f51ad242f853a90a1ccf554cb284
maxmay94 <maxmay94@gmail.com>
2022-11-02 14:37:09 -0400

    cleanup workout page and make workout component

--- src/components/WorkoutTile/WorkoutTile.jsx
+++ src/components/WorkoutTile/WorkoutTile.jsx
@@ -1,7 +1,32 @@
-const WorkoutTile = ({workout}) => {
+import { Link } from 'react-router-dom'
+
+const WorkoutTile = ({workout, exercise, handleSetExercise}) => {
   return (
     <div>
-      
+      <h1 className="underline font-semibold">{workout.name}</h1>
+      <p className="m-5 text-xs text-center">{workout.description}</p>
+      <div className="divide-y divide-slate-800">
+        {
+          workout.exercises &&
+          workout.exercises.map((exercise, j) => (
+            <div key={j} className="text-center mx-3 grid grid-cols-2 py-2 text-sm divide-x divide-slate-800 divide-dashed">
+
+              <Link to={`/exercise/${exercise._id}`} className="h-full w-full">
+                <button onClick={() => handleSetExercise(exercise._id)}>
+                  <div className=" text-amber-500">
+                    {exercise.name}
+                  </div>
+                </button>
+              </Link>
+
+              <div className="">
+                {workout.setsReps[j]}
+              </div>
+
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import WorkoutTile from '../../components/WorkoutTile/WorkoutTile'
 
 const Workouts = ({ workouts, exercise, handleSetExercise }) => {
   return (
@@ -11,30 +12,7 @@ const Workouts = ({ workouts, exercise, handleSetExercise }) => {
         Object.keys(workouts).map((key, i) => {
           return (
             <div key={i} className="text-center m-5 p-2 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl text-slate-200">
-              <h1 className="underline font-semibold">{workouts[key].name}</h1>
-              <p className="m-5 text-xs text-center">{workouts[key].description}</p>
-              <div className="divide-y divide-slate-800">
-                {
-                  workouts[key].exercises &&
-                  workouts[key].exercises.map((exercise, j) => (
-                    <div key={j} className="text-center mx-3 grid grid-cols-2 py-2 text-sm divide-x divide-slate-800 divide-dashed">
-
-                      <Link to={`/exercise/${exercise._id}`} className="h-full w-full">
-                        <button onClick={() => handleSetExercise(exercise._id)}>
-                          <div className=" text-amber-500">
-                            {exercise.name}
-                          </div>
-                        </button>
-                      </Link>
-
-                      <div className="">
-                        {workouts[key].setsReps[j]}
-                      </div>
-
-                    </div>
-                  ))
-                }
-              </div>
+              <WorkoutTile workout={workouts[key]} exercise={exercise} handleSetExercise={handleSetExercise}/>
             </div>
           )
         })
