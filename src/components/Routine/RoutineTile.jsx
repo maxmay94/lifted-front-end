0bda8f420d66a4ba491a938881543ae44afa9b12
maxmay94 <maxmay94@gmail.com>
2022-11-02 13:04:46 -0400

    update reoutineTile style

--- src/components/Routine/RoutineTile.jsx
+++ src/components/Routine/RoutineTile.jsx
@@ -2,11 +2,12 @@ const RoutineTile = ({routine}) => {
   return (
     <div className="text-center m-2 p-1 bg-gradient-to-b from-gray-800 to-slate-700 rounded-xl h-full pt-2 drop-shadow-md">
       <h1 className="text-xl underline"> {routine.name} </h1>
-      <div className="divide-y mt-2 p-3">
+      <p className="text-xs m-5">{routine.description}</p>
+      <div className="divide-y divide-slate-800 px-2 pb-2">
       {
         routine.workouts.map((workout, i) => (
           <div key={i} className="p-2 text-sm">
-            <div className="grid grid-cols-4 divide-x">
+            <div className="grid grid-cols-4 divide-x divide-slate-800 divide-dashed">
               <div className="col-span-1">Day {i+1} </div>
               <div className="col-span-3 text-amber-500">{workout.name}</div>
             </div>
